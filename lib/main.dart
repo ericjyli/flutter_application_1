22077d8f9a16958602666d1fce9b4e2079377d1a
Junyu <ericli.jy@outlook.com>
2023-07-21 15:30:46 +0800

    Finalized the side bar and new page

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for selectedIndex');
@@ -153,6 +153,35 @@ class GeneratorPage extends StatelessWidget {
   }
 }
 
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var favorites = appState.favorites;
+
+    if (favorites.isEmpty) {
+      return Center (
+        child: Text('No favorites yet'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${favorites.length} favorites: '),
+        ),
+        for (var pair in favorites) 
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+    );
+  }
+}
 
 class BigCard extends StatelessWidget {
   const BigCard({
